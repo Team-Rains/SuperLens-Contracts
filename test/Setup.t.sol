fdbfd555e90697256fe2e6cf0557f92e12445ffb
SSDD <0xSSDD@protonmail.com>
2022-12-03 14:35:00 +0200

    adds setup tests to Setup.t

--- test/Setup.t.sol
+++ test/Setup.t.sol
@@ -7,6 +7,12 @@ import "./helpers/Setup.sol";
 
 contract SetupTest is Test, Setup {
 
+  function testSetUp() public {
+    assertTrue(address(Factory) != address(0), "new Factory null");
+    assertTrue(streamManagerImplementation != address(0), "new streamManagerImplementation is null");
+    assertTrue(socialTokenImplementation != address(0), "new socialTokenImplementation is null");
+    assertTrue(stakingContractImplementation != address(0), "new stakingContractImplementation is null");
+  }
   // test createSet
   function testCreateSet () public {
     (address newStreamManager, address newSocialToken, address newStakingContract) = _createSet();
